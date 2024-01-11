d8037bd38672cd1251a0833b8301cd13754c943e
Lep Sistemas <lepsistemas@gmail.com>
2024-01-10 22:10:55 -0300

    Fix indent

--- src/game/presenter/MapPresenter.cs
+++ src/game/presenter/MapPresenter.cs
@@ -9,23 +9,23 @@ public class MapPresenter
 	private readonly Map map;
 	private readonly MapView view;
 
-    public MapPresenter(CameraService cameraService, Map map, MapView view)
+	public MapPresenter(CameraService cameraService, Map map, MapView view)
 	{
 		this.map = map;
 		this.view = view;
 		this.cameraService = cameraService;
 	}
 
-    public void Start()
+	public void Start()
 	{
 		HandleResize();
 		this.view.OnResize += HandleResize;
 	}
 
 	private void HandleResize()
-    {
+	{
 		var size = this.view.GetSize();
 		this.cameraService.SetLimits(size[0], size[1]);
-    }
+	}
 
 }
